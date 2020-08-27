28d06ff3853b7b8f77b1a129df0e8ac756af3246
Brett Stoudt <bstoudt01@gmail.com>
2020-08-27 18:21:14 -0500

    compelted Restriction/Filtering Operations

--- Program.cs
+++ Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace linq
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World This is Linq Stuff!");
+
+            // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+
+            IEnumerable<string> LFruits = from fruit in fruits
+            where fruit.StartsWith("L")
+            select fruit;
+
+            foreach (string fruit in LFruits)
+            {
+                Console.WriteLine($"LFruit '{fruit}'");
+            }
+
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+            {
+                15,
+                8,
+                21,
+                24,
+                32,
+                13,
+                30,
+                12,
+                7,
+                54,
+                48,
+                4,
+                49,
+                96
+            };
+
+            IEnumerable<int> fourSixMultiples = numbers.Where(number => number % 6 == 0 || number % 4 == 0);
+            foreach (int number in fourSixMultiples)
+            {
+                Console.WriteLine($"'{number}' divided by 4 or 6 evenly");
+            }
         }
     }
 }

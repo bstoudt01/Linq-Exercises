bd92f3c8fce08fd56ef2e72aa16ec921fd6a2732
Brett Stoudt <bstoudt01@gmail.com>
2020-08-27 18:44:03 -0500

    Aggregate Operations is completed using sum max and count

--- Program.cs
+++ Program.cs
@@ -106,6 +106,63 @@ namespace linq
             {
                 Console.WriteLine($"'{number}' 1 to 9");
             }
+
+            // Aggregate Operations
+            // Output how many numbers are in this list
+            List<int> aggNumbers = new List<int>()
+            {
+                15,
+                8,
+                21,
+                24,
+                32,
+                13,
+                30,
+                12,
+                7,
+                54,
+                48,
+                4,
+                49,
+                96
+            };
+
+            int AggNumbersCount = aggNumbers.Count;
+            Console.WriteLine($"AggNumbersCount {AggNumbersCount}");
+
+            // How much money have we made?
+            List<double> purchases = new List<double>()
+            {
+                2340.29,
+                745.31,
+                21.76,
+                34.03,
+                4786.45,
+                879.45,
+                9442.85,
+                2454.63,
+                45.65
+            };
+            double totalSpent = purchases.Sum();
+            Console.WriteLine($"totalSpent {totalSpent}");
+
+            // What is our most expensive product?
+            List<double> prices = new List<double>()
+            {
+                879.45,
+                9442.85,
+                2454.63,
+                45.65,
+                2340.29,
+                34.03,
+                4786.45,
+                745.31,
+                21.76
+            };
+            double mostExpensive = prices.Max();
+            Console.WriteLine($"mostExpensive {mostExpensive}");
+
         }
+
     }
 }

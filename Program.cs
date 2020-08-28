e7ed928adf635df6eb31170888a9b21e6433bd11
Brett Stoudt <bstoudt01@gmail.com>
2020-08-27 20:04:04 -0500

    finished the first half of the Using Custom Types exercises

--- Program.cs
+++ Program.cs
@@ -212,6 +212,58 @@ namespace linq
 
             }
 
+            //Using Custom Types
+            // Build a collection of customers who are millionaires
+            //includes public class Customer listed at the bottom of this .cs file
+            List<Customer> customers = new List<Customer>()
+            {
+                new Customer() { Name = "Bob Lesman", Balance = 80345.66, Bank = "FTB" },
+                new Customer() { Name = "Joe Landy", Balance = 9284756.21, Bank = "WF" },
+                new Customer() { Name = "Meg Ford", Balance = 487233.01, Bank = "BOA" },
+                new Customer() { Name = "Peg Vale", Balance = 7001449.92, Bank = "BOA" },
+                new Customer() { Name = "Mike Johnson", Balance = 790872.12, Bank = "WF" },
+                new Customer() { Name = "Les Paul", Balance = 8374892.54, Bank = "WF" },
+                new Customer() { Name = "Sid Crosby", Balance = 957436.39, Bank = "FTB" },
+                new Customer() { Name = "Sarah Ng", Balance = 56562389.85, Bank = "FTB" },
+                new Customer() { Name = "Tina Fey", Balance = 1000000.00, Bank = "CITI" },
+                new Customer() { Name = "Sid Brown", Balance = 49582.68, Bank = "CITI" }
+            };
+
+            List<string> millionaires = new List<string>();
+            System.Collections.IList list = customers;
+            for (int i = 0; i < list.Count; i++)
+            {
+                Customer customer = (Customer) list[i];
+                if (customer.Balance >= 1000000)
+                {
+                    millionaires.Add(customer.Name);
+
+                }
+
+            }
+            foreach (string millionaire in millionaires)
+            {
+                Console.WriteLine($"Another Millionaire: {millionaire}");
+
+            }
+
+            /*
+            Given the same customer set, display how many millionaires per bank.
+            Ref: https://stackoverflow.com/questions/7325278/group-by-in-linq
+
+            Example Output:
+            WF 2
+            BOA 1
+            FTB 1
+            CITI 1
+            */
+
+        }
+        public class Customer
+        {
+            public string Name { get; set; }
+            public double Balance { get; set; }
+            public string Bank { get; set; }
         }
     }
 }
